43f8e84732b73b16d102b67cb3f6b628ea8fa2ee
Tim Pender <mrtimpender@gmail.com>
2016-10-06 23:10:05 -0600

    test

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -282,26 +282,41 @@
         /*==============================================
          Contact form
          ===============================================*/
-        $("form.contact-comments").on("submit", function (e) {
-            e.preventDefault();
-            var sender = $("#email").val();
-            var name = $("#name").val();
-            var subject = $("#subject").val();
-            var message = $("#comments").val();
-            var phone = $("#phone").val();
+         var $contactForm = $('#contactForm');
 
-            var target = $(this).attr("action");
-            var that = this;
-            $.post(target, {name:name,subject:subject,email:sender,phone:phone,comments:message},function(data){
-                if($(that).find(".mailstatus").length==0){
-                    $(that).append($("<div/>").addClass("mailstatus"));
-                }
-                $(".mailstatus").html(data).show();
-                setTimeout(function(){
-                    $(".mailstatus").empty().hide();
-                },5000)
-            });
-        });
+         $contactForm.submit(function(e) {
+         	e.preventDefault();
+         	var $submit = $('input:submit', $contactForm);
+         	var defaultSubmitText = $submit.val();
+
+         	$.ajax({
+         		url: 'https://formspree.io/timpending@gmail.com',
+         		method: 'POST',
+         		data: $(this).serialize(),
+         		dataType: 'json',
+         		beforeSend: function() {
+         			//$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+         			$submit.attr('disabled', true).val('Sending message…');
+         		},
+         		success: function(data) {
+         			//$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+         			$submit.val('Message sent!');
+         			setTimeout(function() {
+         				//$('.alert--success').remove();
+         				$submit.attr('disabled', false).val(defaultSubmitText);
+         			}, 5000);
+         		},
+         		error: function(err) {
+         			//$contactForm.find('.alert--loading').hide();
+         			//$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+         			$submit.val('Ops, there was an error.');
+         			setTimeout(function() {
+         				//$('.alert--error').remove();
+         				$submit.attr('disabled', false).val(defaultSubmitText);
+         			}, 5000);
+         		}
+         	});
+         });
 
     });
 
