93423d22a808a5769aff9717e2dcdc8a6f6c19e6
Tim Pender <mrtimpender@gmail.com>
2016-12-16 13:05:38 -0700

    full update

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy({
+      scrollOffset: 0
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
---
+++ js/main.js
@@ -0,0 +1,40 @@
+// CONTACT HOVER FUNCTIONS
+var showEmailDetails = () => {
+  $('#emailDeets').show('slow');
+}
+
+var hideEmailDetails = () => {
+  $('#emailDeets').hide('slow');
+}
+
+var showPhoneDetails = () => {
+  $('#phoneDeets').show('slow');
+}
+
+var hidePhoneDetails = () => {
+  $('#phoneDeets').hide('slow');
+}
+
+var showGitDetails = () => {
+  $('#gitDeets').show('slow');
+}
+
+var hideGitDetails = () => {
+  $('#gitDeets').hide('slow');
+}
+
+var showLinkDetails = () => {
+  $('#linkDeets').show('slow');
+}
+
+var hideLinkDetails = () => {
+  $('#linkDeets').hide('slow');
+}
+
+var showSpotDetails = () => {
+  $('#spotDeets').show('slow');
+}
+
+var hideSpotDetails = () => {
+  $('#spotDeets').hide('slow');
+}
